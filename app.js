dbbddddc8f14a10544f1a14d1f9f0da3fd327583
Bahare <pondblue07@gmail.com>
2025-10-27 22:10:49 +0330

    added dynamic weather icons
    - Update weather icons based on weather conditions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -14,12 +16,29 @@ async function checkWeather(city) {
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "./assets/images/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "./assets/images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "./assets/images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "./assets/images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "./assets/images/mist.png";
+    }
 }
 
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
-})
+});
 
 
 
